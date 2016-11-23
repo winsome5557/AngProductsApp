f57ab34ed155f72e5c7803df859d9d78b8acbdb6
sarfraz malik <sarfrazmalik@tfl.gov.uk>
2016-11-23 13:40:01 +0000

    onew version# with '#' will be ignored, and an empty message aborts the commit.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,10 @@
 import {Component, OnInit} from '@angular/core';
+import {productService} from './services/productsService'
 
 @Component({
     selector: 'my-app',
-    templateUrl: './app/app.component.html'
+    templateUrl: './app/app.component.html',
+    providers:[productService]
 })
 
 export class AppComponent implements OnInit {
--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -1,13 +1,26 @@
-import { Component } from '@angular/core';
+import { Console } from '@angular/compiler/src/private_import_core';
+import { THIS_EXPR } from '@angular/compiler/src/output/output_ast';
+import { runInThisContext } from 'vm';
+import { Component, OnInit } from '@angular/core';
+import {IProduct} from './productInterfaces';
+import {productService } from './../services/productsService';
+
 
 @Component({
     selector: 'tfl-products',
-    templateUrl: './app/products/products.component.html'
+    templateUrl: './app/products/products.component.html',
 }
 )
-export class productscomponent
+export class productscomponent implements  OnInit
 {
 
+    products: IProduct[];
+
+    // Get the products service injected
+    constructor(private _prodService: productService)
+    {
+    }
+/*
      allproducts: any[] =[
         { 
              name:'product1',
@@ -20,6 +33,13 @@ export class productscomponent
              cost:'15£'
         }
         ];
+*/
 
+    ngOnInit(): void
+    {
+        console.log('initializing products');
+        this.products  =   this._prodService.getAllProducts();
+    }
 
 }
+
